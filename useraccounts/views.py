c710f28e91382c6fcb4ae5b5d335c6637e8afe1f
sohan <btvdhk@gmail.com>
2019-07-24 05:11:25 +0600

    made a full sign up page

--- useraccounts/views.py
+++ useraccounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username = request.POST['username'])
+                return render(request, 'useraccounts/signup.html', {'error':'Username already exist'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
+                auth.login(request,user)
+                return redirect('home')
+        else:
+            return render(request, 'useraccounts/signup.html', {'error':'Password Must Match'})
+    else:
+    
+        return render(request, 'useraccounts/signup.html')
+    
+def login(request):
+    return render(request, 'useraccounts/login.html')
+    
+def logout(request):
+    # todo need to route to homepage
+    return render(request, 'useraccounts/signup.html')
